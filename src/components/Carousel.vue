3bfad15632f8ba5627b98011259e65a31bb70c4c
Jaxson Hood <jaxson.hood@hotmail.com>
2020-07-15 18:52:13 -0400

    commit

---
+++ src/components/Carousel.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-carousel hide-delimiters>
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+      :lazy-src="require('../assets/grey_background.jpg')"
+    >
+      <template v-slot:placeholder>
+        <v-row class="fill-height ma-0" align="center" justify="center">
+          <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+        </v-row>
+      </template>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+export default {
+  name: "Carousel",
+  data() {
+    return {
+      items: [
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg"
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg"
+        }
+      ]
+    };
+  }
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card
+    color="white lighten-4"
+    flat
+    height="200px"
+    tile
+  >
+    <v-toolbar>
+      <v-toolbar-items>
+        <v-btn icon class="align-self-start" size="28" href="/home">
+          <v-icon :color="black">mdi-vector-intersection</v-icon>
+        </v-btn>
+      </v-toolbar-items>
+      <v-toolbar-title>CMXX</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text large href="/home">Home</v-btn>
+      <v-btn text large href="/about">About</v-btn>
+
+      <!-- <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn> -->
+    </v-toolbar>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+    data () {
+      return {}
+    },
+  }
+</script>
