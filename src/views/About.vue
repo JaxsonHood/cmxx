1b749a26d2381d4ef000855e588e620caac0bfea
Jaxson Hood <jaxson.hood@hotmail.com>
2020-07-31 23:39:36 -0400

    d

--- src/views/About.vue
+++ src/views/About.vue
@@ -27,76 +27,63 @@
           max-width="720"
         >We are a team of Computer Scientists seeking to help you achieve your digital platform goals. From full-scale applications, to simple blogs. We can create a scalable, responsive, and innovative solution to the challenges your business faces. Using up and coming web technologies we can meet the demands of an ever-changing online landscape.</v-responsive>
 
-        <v-card
-          :dark="false"
-          class="mx-auto"
-          outlined
-          style="border-radius:20px;"
-          color="grey lighten-3"
-        >
-          <v-row>
-            <v-col
-              v-for="(item,i) in theboys"
-              :key="i"
-              cols="12"
-              sm="4"
-              style="padding:10px;"
+        <v-row>
+          <v-col v-for="(item,i) in theboys" :key="i" cols="12" sm="4" style="padding:5px;">
+            <v-card
+              class="mx-auto my-12"
+              style="border-radius:20px; padding:5px;"
+              max-width="330"
+              :dark="true"
+              outlined
+              elevation="5"
             >
-              <v-card
-                class="mx-auto my-12"
-                style="border-radius:20px; padding:5px;"
-                max-width="330"
-                :dark="true"
-                outlined
-                elevation="5"
-              >
+              <v-row align="center" justify="center" style="height:210px; padding-top:15px;">
+                <v-avatar color="orange" size="170">
+                  <!-- <span class="white--text headline">170</span> -->
+                  <v-img height="200" v-bind:src="require('../assets/' + item.img)"></v-img>
+                </v-avatar>
+              </v-row>
 
-                <v-row align="center" justify="center" style="height:210px; padding-top:15px;">
-                  <v-avatar color="orange" size="170">
-                    <!-- <span class="white--text headline">170</span> -->
-                    <v-img
-                      height="200"
-                      v-bind:src="require('../assets/' + item.img)"
-                    ></v-img>
-                  </v-avatar>
-                </v-row>
+              <v-card-title
+                class="justify-center overline"
+                style="font-size:23px !important;"
+              >{{item.name}}</v-card-title>
+              <v-card-subtitle>
+                <div class="text-center">{{item.role}}</div>
+              </v-card-subtitle>
+              <v-card-text>
+                <div style="color:#EEEEEE;" class="text-center">{{item.writeup}}</div>
+              </v-card-text>
 
-                <v-card-title class="justify-center overline" style="font-size:23px !important;">{{item.name}}</v-card-title>
-                <v-card-subtitle><div class="text-center">{{item.role}}</div></v-card-subtitle>
-                <v-card-text>
-                  <div style="color:#EEEEEE;" class="text-center">{{item.writeup}}</div>
-                </v-card-text>
+              <v-divider class="mx-4"></v-divider>
 
-                <v-divider class="mx-4"></v-divider>
+              <v-card-title>
+                <div style="color:#757575;">SPECIALTIES:</div>
+              </v-card-title>
 
-                <v-card-title><div style="color:#757575;">SPECIALTIES:</div></v-card-title>
+              <v-card-text>
+                <v-chip-group column>
+                  <v-chip
+                    v-for="(skill,j) in item.skills"
+                    :key="j"
+                    :color="skill.color"
+                    outlined
+                  >{{skill.title}}</v-chip>
+                </v-chip-group>
+              </v-card-text>
 
-                <v-card-text>
-                  <v-chip-group
-                    column
-                  >
-                    <v-chip 
-                      v-for="(skill,j) in item.skills"
-                      :key="j"
-                      :color=skill.color
-                      outlined
-                    >{{skill.title}}</v-chip>
-                  </v-chip-group>
-                </v-card-text>
-
-                <v-card-actions>
-                  <!-- <v-btn
+              <v-card-actions>
+                <!-- <v-btn
                     color="deep-purple lighten-2"
                     text
                     @click="reserve"
                   >
                     Reserve
-                  </v-btn> -->
-                </v-card-actions>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-card>
+                </v-btn>-->
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
 
         <div></div>
         <!-- 
@@ -107,7 +94,6 @@
     </v-card>
 
     <div class="py-12"></div>
-    
   </div>
 </template>
 
@@ -126,75 +112,78 @@ export default {
         {
           name: "Jaxson Hood",
           role: "UX Designer / Software Developer",
-          writeup: "As a team member at CMXX I believe in creating products that elevate the client experience, and an enviroment that pushes the team to new heights.",
+          writeup:
+            "As a team member at CMXX I believe in creating products that elevate the client experience, and an enviroment that pushes the team to new heights.",
           img: "jaxson.jpeg",
           skills: [
             {
               title: "UI Conception",
-              color:"light-blue accent-3"
+              color: "light-blue accent-3"
             },
             {
               title: "Clean Design",
-              color:"green accent-3"
+              color: "green accent-3"
             },
             {
               title: "Optimization",
-              color:"red accent-3"
+              color: "red accent-3"
             },
             {
               title: "Proj. Organization",
-              color:"amber accent-3"
-            },
+              color: "amber accent-3"
+            }
           ]
         },
         {
           name: "Cameron Dickie",
           role: "UI Designer / Software Developer",
-          writeup: "As a team member at CMXX I believe in creating products that elevate the client experience, and an enviroment that pushes the team to new heights.",
+          writeup:
+            "As a team member at CMXX I believe in creating products that elevate the client experience, and an enviroment that pushes the team to new heights.",
           img: "placeholder-profile.jpg",
           skills: [
             {
               title: "UI Conception",
-              color:"light-blue accent-3"
+              color: "light-blue accent-3"
             },
             {
               title: "Clean Design",
-              color:"green accent-3"
+              color: "green accent-3"
             },
             {
               title: "Optimization",
-              color:"red accent-3"
+              color: "red accent-3"
             },
             {
               title: "Proj. Organization",
-              color:"amber accent-3"
-            },
+              color: "amber accent-3"
+            }
           ]
         },
         {
           name: "Alex Collins",
           role: "Business Lead / Product Consultant",
-          writeup: "As a team member at CMXX I believe in creating products that elevate the client experience, and an enviroment that pushes the team to new heights.",
+          writeup:
+            "As a team member at CMXX I believe in creating products that elevate the client experience, and an enviroment that pushes the team to new heights.",
           img: "placeholder-profile.jpg",
           skills: [
             {
               title: "UI Conception",
-              color:"light-blue accent-3"
+              color: "light-blue accent-3"
             },
             {
               title: "Clean Design",
-              color:"green accent-3"
+              color: "green accent-3"
             },
             {
               title: "Optimization",
-              color:"red accent-3"
+              color: "red accent-3"
             },
             {
               title: "Proj. Organization",
-              color:"amber accent-3"
-            },
+              color: "amber accent-3"
+            }
           ]
-        },
+        }
       ]
     };
   }
