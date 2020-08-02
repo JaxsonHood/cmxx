35f01e71463437f666610750681ea120693d98b2
Jaxson Hood <jaxson.hood@hotmail.com>
2020-08-02 14:57:20 -0400

    push

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,9 +2,9 @@
   <v-card
     outlined
     height="74px"
-    class="grey lighten-1"
+    
     style="border-radius:15px; margin: auto;"
-    max-width="1455"
+    max-width="1450"
     :dark="true"
   >
     <v-toolbar class="elevation-0" height="72px" :dark="true" max-width="1455">
@@ -36,10 +36,23 @@
           <v-icon left>smartphone</v-icon>
           Apps
         </v-chip>
+
+        <v-chip
+          class="ma-2"
+          color="#00E676"
+          outlined
+        >
+          <v-icon left>offline_bolt</v-icon>
+          Launch
+        </v-chip>
       </div>
 
+      <div style="width:5px;"></div>
+
       <v-btn id='Home' text large style="border-radius:10px;" :elevation="5" href="/home">Home</v-btn>
+      <div style="width:3px;"></div>
       <v-btn id='About' text large style="border-radius:10px;" :elevation="5" href="/about">About</v-btn>
+      <div style="width:3px;"></div>
       <v-btn id='Contact' text large style="border-radius:10px;" :elevation="5" href="/contact">Contact</v-btn>
 
       <!-- <v-btn icon>
